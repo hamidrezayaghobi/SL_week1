ff22b63efa94d6e128d03108f11fbcd17ae5f507
alirezaheidari-cs <alirezaheidari.cs@gmail.com>
2023-10-31 00:01:33 +0330

    add prints for monitoring

--- arithmetic.py
+++ arithmetic.py
@@ -1,13 +1,28 @@
-def subtract(x, y):
+def subtract(x, y, monitor=False):
+    if monitor:
+	print(f'x = {x}, y = {y}')
+        return float(x) - float(y)
     return float(x) - float(y)
-
-def add(x, y):
+def add(x, y, monitor=False):
+    if monitor:
+        print(f'x = {x}, y = {y}')
+        return float(x) + float(y)
     return float(x) + float(y)
 
-def multiply(x, y):
+def multiply(x, y, monitor=False):
+    if monitor:
+        print(f'x = {x}, y = {y}')
+        return float(x) * float(y)
     return float(x) * float(y)
 
-def divide(x, y):
+def divide(x, y, monitor=False):
+    if monitor:
+        print(f'x = {x}, y = {y}')
+	if y == 0:
+            raise "division by zero"
+
+        return float(x) /  float(y)
+
     if y == 0:
 	raise "division by zero"
     return float(x) / float(y)
