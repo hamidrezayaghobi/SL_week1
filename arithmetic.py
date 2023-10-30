3e4fbefe2e471afe7653655c635a8743ae2f8e65
alirezaheidari-cs <alirezaheidari.cs@gmail.com>
2023-10-30 23:58:22 +0330

    make float conversion cleaner

--- arithmetic.py
+++ arithmetic.py
@@ -1,13 +1,12 @@
 def subtract(x, y):
-    return float(x) - float(y)
-
+    return 1.0 * x - y
 def add(x, y):
-    return float(x) + float(y)
+    return 1.0 * x + 1.0 * y
 
 def multiply(x, y):
-    return float(x) * float(y)
+    return 1.0 * x * y
 
 def divide(x, y):
     if y == 0:
 	raise "division by zero"
-    return float(x) / float(y)
+    return 1.0 * x / y
