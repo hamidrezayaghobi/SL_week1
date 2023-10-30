38fe836aed540ea74e193357a6de9cebc3399c8e
hamidrezayaghobi <hamidrezayaghobi021@gmail.com>
2023-10-30 22:24:11 +0330

    add subtract

--- arithmetic.py
+++ arithmetic.py
@@ -4,3 +4,10 @@ def add(x, y):
     x = int(x)
     y = int(y)
     return x + y
+
+def subtract(x, y):
+    x = int(x)
+    y = int(y)
+    return x - y
+
+
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from input_functions import get_two_inputs
 x, y = get_two_inputs()
 
 print(f"The sum of {x} and {y} is:", add(x, y))
-
+print(f"The difference between {x} and {y} is:", subtract(x, y))
