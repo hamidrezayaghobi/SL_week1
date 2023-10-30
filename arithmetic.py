724bd258935dec8615e5d4631646606761dde143
alirezaheidari-cs <alirezaheidari.cs@gmail.com>
2023-10-31 00:37:17 +0330

    add a logger module for furthur usage

--- arithmetic.py
+++ arithmetic.py
@@ -1,20 +1,22 @@
-def subtract(x, y, monitor=False):
+from .logger import log_str
+
+def subtract(x, y, logger=log_str):
     if monitor:
-	print(f'x = {x}, y = {y}')
+	logger(f'x = {x}, y = {y}')
     return 1.0 * x - y
-def add(x, y, monitor=False):
+def add(x, y, logger=log_str):
     if monitor:
-        print(f'x = {x}, y = {y}')
+        logger(f'x = {x}, y = {y}')
     return 1.0 * x + y
 
-def multiply(x, y, monitor=False):
+def multiply(x, y, logger=log_str):
     if monitor:
-        print(f'x = {x}, y = {y}')
+        logger(f'x = {x}, y = {y}')
     return 1.0 * x *  y
 
-def divide(x, y, monitor=False):
+def divide(x, y, logger=log_str):
     if monitor:
-        print(f'x = {x}, y = {y}')
+        logger(f'x = {x}, y = {y}')
     if y == 0:
 	raise "division by zero"
     return 1.0 * x / y
